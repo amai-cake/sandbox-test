bd5b6562f805ad778ce0d5445f39733f882f2a16
amai-cake <testbobbshop@gmail.com>
2023-04-04 13:40:58 +0000

    Update 7 files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "./styles.css";
+/**
+ * const
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-console.log("aaaa");
+var val1 = "var変数";
+console.log(val1);
+
+//
+
+val1 = "上書き";
+console.log(val1);
